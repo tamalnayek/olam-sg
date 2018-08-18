bdd079a901a6161a5fe77d15badc1ca7f2630f5e
imnag.trainer@gmail.com <nagabhushanamn@gmail.com>
2018-08-18 18:59:18 +0800

    spring intro

--- bank-app-v2 (xml)/src/main/java/com/bank/repository/JdbcAccountRepository.java
+++ bank-app-v2 (xml)/src/main/java/com/bank/repository/JdbcAccountRepository.java
@@ -1,8 +1,13 @@
 package com.bank.repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.bank.model.Account;
 
@@ -10,20 +15,29 @@ public class JdbcAccountRepository implements AccountRepository {
 
 	private static Logger logger = Logger.getLogger("bank");
 
-	private DataSource dataSource;
+	private JdbcTemplate jdbcTemplate;
 
 	public JdbcAccountRepository(DataSource dataSource) {
-		this.dataSource = dataSource;
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 		logger.info("JdbcAccountRepository instance created...");
 	}
 
 	public Account load(String num) {
 		logger.info("loading account - " + num);
-		return null;
+		String sql = "select * from ACCOUNT where num=?";
+		return jdbcTemplate.queryForObject(sql, new RowMapper<Account>() {
+			public Account mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Account account = new Account();
+				account.setNum(rs.getString("num"));
+				account.setBalance(rs.getDouble("balance"));
+				return account;
+			}
+		}, num);
 	}
 
 	public void update(Account account) {
 		logger.info("updating account");
+		jdbcTemplate.update("update ACCOUNT set balance=? where num=?", account.getBalance(), account.getNum());
 	}
 
 }
