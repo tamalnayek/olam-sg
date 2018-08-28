dc4cbf03b68d258b3491aef9993422184c5977bc
imnag.trainer@gmail.com <nagabhushanamn@gmail.com>
2018-08-28 16:35:13 +0800

    rest api

--- PM-api-service/src/main/java/com/olam/pm/web/ProductController.java
+++ PM-api-service/src/main/java/com/olam/pm/web/ProductController.java
@@ -1,10 +1,18 @@
 package com.olam.pm.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.olam.pm.model.Product;
@@ -17,11 +25,37 @@ public class ProductController {
 	@Autowired
 	private ProductRepository productRepository;
 
-	// @RequestMapping(method=RequestMethod.POST)
-	// or
 	@PostMapping
-	public void create(@RequestBody Product product) {
-		productRepository.save(product);
+	public ResponseEntity<Product> create(@RequestBody Product product) {
+		product = productRepository.save(product);
+		return new ResponseEntity<Product>(product, HttpStatus.CREATED);
+	}
+
+	@GetMapping
+	public ResponseEntity<List<Product>> get() {
+		List<Product> products = productRepository.findAll();
+		return new ResponseEntity<List<Product>>(products, HttpStatus.OK);
+	}
+
+	@GetMapping(value = "{id}")
+	public ResponseEntity<?> getOne(@PathVariable int id) {
+		Optional<Product> optional = productRepository.findById(id);
+		if (optional.isPresent())
+			return new ResponseEntity<Product>(optional.get(), HttpStatus.OK);
+		else
+			return new ResponseEntity<String>("product not exist", HttpStatus.NOT_FOUND);
+	}
+
+	@PutMapping(value = "{id}")
+	public ResponseEntity<Product> update(@PathVariable int id, @RequestBody Product product) {
+		product = productRepository.save(product);
+		return new ResponseEntity<Product>(product, HttpStatus.OK);
+	}
+
+	@DeleteMapping(value = "{id}")
+	public ResponseEntity<String> deleteOne(@PathVariable int id) {
+		productRepository.deleteById(id);
+		return new ResponseEntity<String>("product deleted", HttpStatus.OK);
 	}
 
 }
